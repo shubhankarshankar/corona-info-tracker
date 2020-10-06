b0ba96e3dd6f84fcb80fadc0d4399ab20bf4ca73
shubhankarshankar <shubhankarshankar@gmail.com>
2020-10-07 01:16:59 +0530

    ADD Logo

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,39 @@
 import React from "react";
+import logo from "../img/logo 2.png";
 
 class SearchBar extends React.Component {
-  state = { input: "" };
+	state = { input: "" };
 
-  onInputChange = event => {
-    this.setState({ input: event.target.value });
-  };
+	onInputChange = event => {
+		this.setState({ input: event.target.value });
+	};
 
-  onFormSubmit = event => {
-    event.preventDefault();
+	onFormSubmit = event => {
+		event.preventDefault();
 
-    this.state.input === ""
-      ? this.setState({ input: "" })
-      : this.props.onSearchSubmit(this.state.input);
-  };
+		this.state.input === ""
+			? this.setState({ input: "" })
+			: this.props.onSearchSubmit(this.state.input);
+	};
 
-  render() {
-    return (
-      <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Search By Country</label>
-            <input
-              placeholder="Enter Country"
-              type="text"
-              value={this.state.input}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="ui segment search-wrapper">
+				<img className="ui centered image" src={logo} alt="logo" />
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<div className="field">
+						<label>Search By Country</label>
+						<input
+							placeholder="Enter Country"
+							type="text"
+							value={this.state.input}
+							onChange={this.onInputChange}
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
